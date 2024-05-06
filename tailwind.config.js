9682f77230aa180a019757934c31e6f2d8366fe3
Roman Melnyk <email.rom.melnyk@gmail.com>
2024-05-06 11:23:05 +0200

    Improve styling for the dark mode

--- tailwind.config.js
+++ tailwind.config.js
@@ -11,36 +11,36 @@ module.exports = {
       transparent: "transparent",
       current: "currentColor",
       accent: {
-        "light": "hsl(195, 80%, 75%)",
-        "dark": "#54d7f3",
+        "light": "hsl(195, 80%, 55%)",
+        "dark": "hsl(195, 80%, 55%)",
         high: {
-          "light": "hsl(195, 80%, 55%)",
-          "dark": "#001f25",
+          "light": "hsl(195, 80%, 35%)",
+          "dark": "hsl(195, 60%, 75%)",
         },
       },
       surface: {
-        "light": "hsl(195, 3%, 100%)",
-        "dark": "#001f25",
+        "light": "hsl(195, 3%, 95%)",
+        "dark": "hsl(195, 65%, 10%)",
         high: {
-          "light": "hsl(195, 50%, 95%)",
-          "dark": "#001f25",
+          "light": "hsl(195, 50%, 90%)",
+          "dark": "hsl(195, 65%, 15%)",
         },
       },
       on: {
         accent: {
           "light": "hsl(195, 85%, 10%)",
-          "dark": "#003640",
+          "dark": "hsl(195, 3%, 95%)",
           high: {
-            "light": "hsl(195, 90%, 15%)",
-            "dark": "#001f25",
+            "light": "hsl(195, 3%, 95%)",
+            "dark": "hsl(195, 40%, 25%)",
           },
         },
         surface: {
           "light": "hsl(195, 65%, 7%)",
-          "dark": "#a6eeff",
+          "dark": "hsl(195, 3%, 95%)",
           high: {
             "light": "hsl(195, 85%, 10%)",
-            "dark": "#001f25",
+            "dark": "hsl(195, 50%, 90%)",
           },
         },
       },
